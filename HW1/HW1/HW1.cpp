609bf71e25b04f7d80156f661573f113b5cc6390
keil <keilbroing@hotmail.com>
2015-04-08 09:20:29 -0700

    added a switch case and functions to clean up code

--- HW1/HW1/HW1.cpp
+++ HW1/HW1/HW1.cpp
@@ -31,21 +31,8 @@ struct node{
 	string state_key = "";
 };
 
-void print_node(node temp){
-	//cout << temp.state.left_missionaries << "," << temp.state.left_cannibals << "," << temp.state.left_boats << "," << temp.state.right_missionaries
-	//	<< "," << temp.state.right_cannibals << "," << temp.state.right_boats << "," << "    depth=" << temp.depth << "		parent=" 
-	//	<< temp.parent_node.left_missionaries << "," << temp.parent_node.left_cannibals << "," << temp.parent_node.left_boats << "," << temp.parent_node.right_missionaries
-	//	<< "," << temp.parent_node.right_cannibals << "," << temp.parent_node.right_boats << "," << endl;
-	//ofstream myfile;
-	//myfile.open("mcb_log.txt",ios::app);
-	//myfile << temp.state.left_missionaries << "," << temp.state.left_cannibals << "," << temp.state.left_boats << "," << temp.state.right_missionaries
-	//	<< "," << temp.state.right_cannibals << "," << temp.state.right_boats << "," << "    depth=" << temp.depth << "		parent="
-	//	<< temp.parent_node.left_missionaries << "," << temp.parent_node.left_cannibals << "," << temp.parent_node.left_boats << "," << temp.parent_node.right_missionaries
-	//	<< "," << temp.parent_node.right_cannibals << "," << temp.parent_node.right_boats << "," << endl;
-	//myfile.close();
 
-}
-void print_node2(node temp){
+void print_node(node temp){
 	cout << temp.state.left_missionaries << "," << temp.state.left_cannibals << "," << temp.state.left_boats << "," << temp.state.right_missionaries
 		<< "," << temp.state.right_cannibals << "," << temp.state.right_boats << "," << "    depth=" << temp.depth <<
 		"		parent="
@@ -66,13 +53,6 @@ string state_string(node temp){
 	string temp_string = "";
 	temp_string =  to_string( temp.state.left_missionaries )  + ","  + to_string( temp.state.left_cannibals )  + ","  + to_string( temp.state.left_boats )  + ","  + to_string( temp.state.right_missionaries
 		)  + ","  + to_string( temp.state.right_cannibals ) + ","  + to_string( temp.state.right_boats );   
-	//ofstream myfile;
-	//myfile.open("mcb_log.txt",ios::app);
-	//myfile );  + to_string( temp.state.left_missionaries );  + ","  + to_string( temp.state.left_cannibals );  + ","  + to_string( temp.state.left_boats );  + ","  + to_string( temp.state.right_missionaries
-	//	);  + ","  + to_string( temp.state.right_cannibals );  + ","  + to_string( temp.state.right_boats );  + ","  + to_string( "    depth=" );  + to_string( temp.depth );  + to_string( "		parent="
-	//	);  + to_string( temp.parent_node.left_missionaries );  + ","  + to_string( temp.parent_node.left_cannibals );  + ","  + to_string( temp.parent_node.left_boats );  + ","  + to_string( temp.parent_node.right_missionaries
-	//	);  + ","  + to_string( temp.parent_node.right_cannibals );  + ","  + to_string( temp.parent_node.right_boats );  + ","  + to_string( endl;
-	//myfile.close();
 	return temp_string;
 }
 bool nodePassed(node current, node goal){
@@ -89,47 +69,18 @@ bool nodePassed(node current, node goal){
 		return false;
 }
 
-
-bool general_search(int problem, deque<node> &myqueue){
-
-	//create queue
-	node starting_node;
-	node goal_node;
-	node nd;
-	set<string> myset;
-	goal_node.state.left_missionaries = 3;
-	goal_node.state.left_cannibals = 3;
-	goal_node.state.left_boats = 1;
-	goal_node.state.right_missionaries = 0;
-	goal_node.state.right_cannibals = 0;
-	goal_node.state.right_boats = 0;
+bool ExpandNode(node nd, int successor, node &new_node){
 
 
-	starting_node.state.left_missionaries = 0;
-	starting_node.state.left_cannibals = 0;
-	starting_node.state.left_boats = 0;
-	starting_node.state.right_missionaries = 3;
-	starting_node.state.right_cannibals = 3;
-	starting_node.state.right_boats = 1;
-	starting_node.state_key = state_string(starting_node);
-	//starting_node.parents = state_string(starting_node);
-	auto result_set = myset.insert(starting_node.state_key);
-	myqueue.push_back(starting_node);
-
-
-	cout << "myquue contains:" << endl;
-	while (!myqueue.empty()){
-		nd = myqueue.front();
-		print_node(nd);
-
+	switch (successor){
+	case 1:
 		///////////
 		//Check 1. Put one missionary in the boat
-
+		new_node = nd;
 		//boat on right
 		if (nd.state.right_boats == 1 && nd.state.right_missionaries >= 1){
 			if (((nd.state.left_missionaries + 1 == 0) || (nd.state.left_missionaries + 1 >= nd.state.left_cannibals)) &&
-				((nd.state.right_missionaries - 1 == 0 ) || (nd.state.right_missionaries - 1 >= nd.state.right_cannibals))){
-				node new_node = nd;
+				((nd.state.right_missionaries - 1 == 0) || (nd.state.right_missionaries - 1 >= nd.state.right_cannibals))){
 				new_node.state.left_missionaries += 1;
 				new_node.state.right_missionaries -= 1;
 				new_node.state.left_boats = 1;
@@ -138,19 +89,13 @@ bool general_search(int problem, deque<node> &myqueue){
 				//new_node.parent_node = nd.state;
 				//new_node.parents = state_string(new_node) + "<--" + nd.parents;
 				new_node.state_key = state_string(new_node);
-				result_set = myset.insert(new_node.state_key);
-				if (result_set.second)
-					myqueue.push_back(new_node);
-				if (nodePassed(new_node, goal_node)){
-					return true;
-				}
+
 			}
 		}
 		//boat on left
-		else if(nd.state.left_boats == 1 && nd.state.left_missionaries >= 1){
-			if (((nd.state.left_missionaries - 1 == 0 ) || (nd.state.left_missionaries - 1 >= nd.state.left_cannibals)) &&
+		else if (nd.state.left_boats == 1 && nd.state.left_missionaries >= 1){
+			if (((nd.state.left_missionaries - 1 == 0) || (nd.state.left_missionaries - 1 >= nd.state.left_cannibals)) &&
 				((nd.state.right_missionaries + 1 == 0) || (nd.state.right_missionaries + 1 >= nd.state.right_cannibals))){
-				node new_node = nd;
 				new_node.state.left_missionaries -= 1;
 				new_node.state.right_missionaries += 1;
 				new_node.state.left_boats = 0;
@@ -159,22 +104,19 @@ bool general_search(int problem, deque<node> &myqueue){
 				//new_node.parent_node = nd.state;
 				//new_node.parents = state_string(new_node) + "<--" + nd.parents;
 				new_node.state_key = state_string(new_node);
-				result_set = myset.insert(new_node.state_key);
-				if (result_set.second)
-					myqueue.push_back(new_node);
-				if (nodePassed(new_node, goal_node)){
-					return true;
-				}
+
 			}
 		}
+		break;
+
+	case 2:
 		///////////
 		//Check 2. Put two missionary in the boat
 
 		//boat on right
 		if (nd.state.right_boats == 1 && nd.state.right_missionaries >= 2){
-			if (((nd.state.left_missionaries  + 2 == 0) || (nd.state.left_missionaries + 2 >= nd.state.left_cannibals)) &&
+			if (((nd.state.left_missionaries + 2 == 0) || (nd.state.left_missionaries + 2 >= nd.state.left_cannibals)) &&
 				((nd.state.right_missionaries - 2 == 0) || (nd.state.right_missionaries - 2 >= nd.state.right_cannibals))){
-				node new_node = nd;
 				new_node.state.left_missionaries += 2;
 				new_node.state.right_missionaries -= 2;
 				new_node.state.left_boats = 1;
@@ -182,22 +124,14 @@ bool general_search(int problem, deque<node> &myqueue){
 				new_node.depth += 1;
 				//new_node.parent_node = nd.state;
 				//new_node.parents = state_string(new_node) + "<--" + nd.parents;
-								new_node.state_key = state_string(new_node);
+				new_node.state_key = state_string(new_node);
 
-				result_set = myset.insert(new_node.state_key);
-
-				if (result_set.second)
-					myqueue.push_back(new_node);
-				if (nodePassed(new_node, goal_node)){
-					return true;
-				}
 			}
 		}
 		//boat on left
-		  else if(nd.state.left_boats == 1 && nd.state.left_missionaries >= 2){
+		else if (nd.state.left_boats == 1 && nd.state.left_missionaries >= 2){
 			if (((nd.state.left_missionaries - 2 == 0) || (nd.state.left_missionaries - 2 >= nd.state.left_cannibals)) &&
 				((nd.state.right_missionaries + 2 == 0) || (nd.state.right_missionaries + 2 >= nd.state.right_cannibals))){
-				node new_node = nd;
 				new_node.state.left_missionaries -= 2;
 				new_node.state.right_missionaries += 2;
 				new_node.state.left_boats = 0;
@@ -205,26 +139,21 @@ bool general_search(int problem, deque<node> &myqueue){
 				new_node.depth += 1;
 				//new_node.parent_node = nd.state;
 				//new_node.parents = state_string(new_node) + "<--" + nd.parents;
-								new_node.state_key = state_string(new_node);
+				new_node.state_key = state_string(new_node);
 
-				result_set = myset.insert(new_node.state_key);
 
-				if (result_set.second)
-					myqueue.push_back(new_node);
-				if (nodePassed(new_node, goal_node)){
-					return true;
-				}
 			}
 		}
+		break;
 
+	case 3:
 		///////////
 		//Check 3. Put one cannibal in the boat
 
 		//boat on right
-		 if (nd.state.right_boats == 1 && nd.state.right_cannibals >= 1){
+		if (nd.state.right_boats == 1 && nd.state.right_cannibals >= 1){
 			if (((nd.state.left_missionaries == 0) || (nd.state.left_missionaries >= nd.state.left_cannibals + 1)) &&
 				((nd.state.right_missionaries == 0) || (nd.state.right_missionaries >= nd.state.right_cannibals - 1))){
-				node new_node = nd;
 				new_node.state.left_cannibals += 1;
 				new_node.state.right_cannibals -= 1;
 				new_node.state.left_boats = 1;
@@ -232,22 +161,15 @@ bool general_search(int problem, deque<node> &myqueue){
 				new_node.depth += 1;
 				//new_node.parent_node = nd.state;
 				//new_node.parents = state_string(new_node) + "<--" + nd.parents;
-								new_node.state_key = state_string(new_node);
+				new_node.state_key = state_string(new_node);
 
-				result_set = myset.insert(new_node.state_key);
 
-				if (result_set.second)
-					myqueue.push_back(new_node);
-				if (nodePassed(new_node, goal_node)){
-					return true;
-				}
 			}
 		}
 		//boat on left
-		  else if(nd.state.left_boats == 1 && nd.state.left_cannibals >= 1){
-			if (((nd.state.left_missionaries == 0 ) || (nd.state.left_missionaries >= nd.state.left_cannibals - 1)) &&
-				((nd.state.right_missionaries == 0 ) || (nd.state.right_missionaries >= nd.state.right_cannibals + 1))){
-				node new_node = nd;
+		else if (nd.state.left_boats == 1 && nd.state.left_cannibals >= 1){
+			if (((nd.state.left_missionaries == 0) || (nd.state.left_missionaries >= nd.state.left_cannibals - 1)) &&
+				((nd.state.right_missionaries == 0) || (nd.state.right_missionaries >= nd.state.right_cannibals + 1))){
 				new_node.state.left_cannibals -= 1;
 				new_node.state.right_cannibals += 1;
 				new_node.state.left_boats = 0;
@@ -255,26 +177,21 @@ bool general_search(int problem, deque<node> &myqueue){
 				new_node.depth += 1;
 				//new_node.parent_node = nd.state;
 				//new_node.parents = state_string(new_node) + "<--" + nd.parents;
-								new_node.state_key = state_string(new_node);
+				new_node.state_key = state_string(new_node);
 
-				result_set = myset.insert(new_node.state_key);
 
-				if (result_set.second)
-					myqueue.push_back(new_node);
-				if (nodePassed(new_node, goal_node)){
-					return true;
-				}
 			}
 		}
+		break;
 
+	case 4:
 		///////////
 		//Check 4. Put one cannibal and one missionary in the boat
 
 		//boat on right
- if (nd.state.right_boats == 1 && nd.state.right_missionaries >= 1 && nd.state.right_cannibals >= 1 ){
+		if (nd.state.right_boats == 1 && nd.state.right_missionaries >= 1 && nd.state.right_cannibals >= 1){
 			if (((nd.state.left_missionaries + 1 == 0) || (nd.state.left_missionaries + 1 >= nd.state.left_cannibals + 1)) &&
 				((nd.state.right_missionaries - 1 == 0) || (nd.state.right_missionaries - 1 >= nd.state.right_cannibals - 1))){
-				node new_node = nd;
 				new_node.state.left_missionaries += 1;
 				new_node.state.right_missionaries -= 1;
 				new_node.state.left_cannibals += 1;
@@ -284,23 +201,16 @@ bool general_search(int problem, deque<node> &myqueue){
 				new_node.depth += 1;
 				//new_node.parent_node = nd.state;
 				//new_node.parents = state_string(new_node) + "<--" + nd.parents;
-								new_node.state_key = state_string(new_node);
+				new_node.state_key = state_string(new_node);
 
-				result_set = myset.insert(new_node.state_key);
 
-				if (result_set.second)
-					myqueue.push_back(new_node);
-				if (nodePassed(new_node, goal_node)){
-					return true;
-				}
 			}
 		}
 		//boat on left
-		  else if(nd.state.left_boats == 1 && nd.state.left_missionaries >= 1 && nd.state.left_cannibals >= 1){
-			if (((nd.state.left_missionaries  - 1 == 0) || (nd.state.left_missionaries - 1 >= nd.state.left_cannibals - 1)) &&
+		else if (nd.state.left_boats == 1 && nd.state.left_missionaries >= 1 && nd.state.left_cannibals >= 1){
+			if (((nd.state.left_missionaries - 1 == 0) || (nd.state.left_missionaries - 1 >= nd.state.left_cannibals - 1)) &&
 				((nd.state.right_missionaries + 1 == 0) || (nd.state.right_missionaries + 1 >= nd.state.right_cannibals + 1)))
 			{
-				node new_node = nd;
 				new_node.state.left_missionaries -= 1;
 				new_node.state.right_missionaries += 1;
 				new_node.state.left_cannibals -= 1;
@@ -310,19 +220,13 @@ bool general_search(int problem, deque<node> &myqueue){
 				new_node.depth += 1;
 				//new_node.parent_node = nd.state;
 				//new_node.parents = state_string(new_node) + "<--" + nd.parents;
-								new_node.state_key = state_string(new_node);
+				new_node.state_key = state_string(new_node);
 
-				result_set = myset.insert(new_node.state_key);
-
-				if (result_set.second)
-					myqueue.push_back(new_node);
-				if (nodePassed(new_node, goal_node)){
-					return true;
-				}
 			}
 		}
+		break;
 
-
+	case 5:
 		///////////
 		//Check 5. Put two cannibal in the boat
 
@@ -330,7 +234,6 @@ bool general_search(int problem, deque<node> &myqueue){
 		if (nd.state.right_boats == 1 && nd.state.right_cannibals >= 2){
 			if (((nd.state.left_missionaries == 0) || (nd.state.left_missionaries >= nd.state.left_cannibals + 2)) &&
 				((nd.state.right_missionaries == 0) || (nd.state.right_missionaries >= nd.state.right_cannibals - 2))){
-				node new_node = nd;
 				new_node.state.left_cannibals += 2;
 				new_node.state.right_cannibals -= 2;
 				new_node.state.left_boats = 1;
@@ -340,45 +243,61 @@ bool general_search(int problem, deque<node> &myqueue){
 
 
 				//new_node.parents = state_string(new_node) + "<--" + nd.parents;
-								new_node.state_key = state_string(new_node);
+				new_node.state_key = state_string(new_node);
 
-				result_set = myset.insert(new_node.state_key);
 
-				if (result_set.second)
-					myqueue.push_back(new_node);
-				if (nodePassed(new_node, goal_node)){
-					return true;
-				}
 			}
 		}
 		//boat on left
-		  else if(nd.state.left_boats == 1 && nd.state.left_cannibals >= 2){
+		else if (nd.state.left_boats == 1 && nd.state.left_cannibals >= 2){
 			if (((nd.state.left_missionaries == 0) || (nd.state.left_missionaries >= nd.state.left_cannibals - 2)) &&
 				((nd.state.right_missionaries == 0) || (nd.state.right_missionaries >= nd.state.right_cannibals + 2))){
-				node new_node = nd;
 				new_node.state.left_cannibals -= 2;
 				new_node.state.right_cannibals += 2;
 				new_node.state.left_boats = 0;
 				new_node.state.right_boats = 1;
 				new_node.depth += 1;
-				//new_node.parent_node = nd.state;
-
-				//new_node.parents = state_string(new_node) + "<--" + nd.parents;
-								new_node.state_key = state_string(new_node);
-
-				result_set = myset.insert(new_node.state_key);
-
-				if (result_set.second)
-					myqueue.push_back(new_node);
-				if (nodePassed(new_node, goal_node)){
-					return true;
-				}
+				new_node.state_key = state_string(new_node);
 			}
 		}
-
-
-		myqueue.pop_front();
+		break;
+	default:
+		cout << "no case mattched given successor number";
 	}
+	return false;
+}
+
+bool bfs(node starting_node, node goal_node, deque<node> &fringe){
+	set<string> myset;
+	node nd;
+
+	auto result_set = myset.insert(starting_node.state_key);
+	fringe.push_back(starting_node);
+	while (!fringe.empty()){
+		node new_node = nd;
+		nd = fringe.front();
+		print_node(nd);
+
+
+
+		ExpandNode(nd, 0, new_node);
+
+
+		result_set = myset.insert(new_node.state_key);
+		if (result_set.second)
+			fringe.push_back(new_node);
+		if (nodePassed(new_node, goal_node)){
+			return true;
+		}
+		fringe.pop_front();
+	}
+	return false;
+}
+
+bool general_search(int problem, deque<node> &myqueue){
+
+	//create queue
+
 	//while (!myqueue.empty()){
 	//	node temp = myqueue.front();
 	//	print_node(temp);
@@ -393,6 +312,24 @@ bool general_search(int problem, deque<node> &myqueue){
 int main(int argc, _TCHAR* argv[])
 {
 	deque<node> some_queue;
+	node starting_node;
+	node goal_node;
+
+	goal_node.state.left_missionaries = 100;
+	goal_node.state.left_cannibals = 90;
+	goal_node.state.left_boats = 1;
+	goal_node.state.right_missionaries = 0;
+	goal_node.state.right_cannibals = 0;
+	goal_node.state.right_boats = 0;
+
+
+	starting_node.state.left_missionaries = 0;
+	starting_node.state.left_cannibals = 0;
+	starting_node.state.left_boats = 0;
+	starting_node.state.right_missionaries = 100;
+	starting_node.state.right_cannibals = 90;
+	starting_node.state.right_boats = 1;
+	starting_node.state_key = state_string(starting_node);
 
 	if (general_search(5,some_queue)){
 		print_node(some_queue.front());
@@ -400,7 +337,7 @@ int main(int argc, _TCHAR* argv[])
 
 
 		node temp = some_queue.back();
-		print_node2(temp);
+		print_node(temp);
 		some_queue.pop_front();
 
 	}
